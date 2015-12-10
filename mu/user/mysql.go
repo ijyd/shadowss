9488ad540994152d4473f3f844159570dccdffa2
orvice <orvice@gmail.com>
2015-12-11 01:01:17 +0800

    update mysql boot

--- mu/user/mysql.go
+++ mu/user/mysql.go
@@ -1,17 +1,27 @@
 package user
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 func NewMysqlClient() Client {
 	var client *MysqlClient
 	return client
 }
 
 type MysqlClient struct {
+	db    gorm.DB
+	table string
 }
 
 type MysqlUser struct {
 	port   int
 	passwd string
 	method string
+}
+
+func (c MysqlUser) TableName() string {
+	return tableName
 }
 
 func (u *MysqlUser) GetPort() int {
@@ -32,6 +42,17 @@ func (u *MysqlUser) UpdatetTraffic() error {
 
 func (c *MysqlClient) GetUsers() ([]User, error) {
 	var datas []*MysqlUser
+	rows, err := c.db.Model(MysqlUser{}).Where("enable = ?", "1").Select("passwd, prot, method").Rows()
+	if err != nil {
+		var users []User
+		return users, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var data *MysqlUser
+		rows.Scan(data.passwd, data.port, data.method)
+		datas = append(datas, data)
+	}
 	users := make([]User, len(datas))
 	for k, v := range datas {
 		users[k] = v
---
+++ mu/user/mysql_boot.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+var (
+	tableName string
+)
+
+func genConnStr(user, password, host, dbname string) string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=True", user, password, host, dbname)
+}
+
+func (c *MysqlClient) Boot(dbType, user, password, host, dbname string) error {
+	var err error
+	c.db, err = gorm.Open(dbType, genConnStr(user, password, host, dbname))
+	if err != nil {
+		return err
+	}
+	c.db.DB().Ping()
+	return nil
+}
+
+func (c *MysqlClient) SetTable(table string) {
+	tableName = table
+	c.table = table
+}
